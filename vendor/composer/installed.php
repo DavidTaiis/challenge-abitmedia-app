6759402ae96e48df57b4dbef0e0a140cf6468a67
David <edtg17@hotmail.com>
2024-01-11 13:42:20 -0500

    Collection postman

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e4cf838faa688e91e7801752ea24f5c81c10f63c',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -491,9 +491,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e4cf838faa688e91e7801752ea24f5c81c10f63c',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
